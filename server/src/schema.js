2669fc85ba40660b3a941906fa4e32c113811b67
bodoganabel <bodoganabel@gmail.com>
2021-04-12 10:58:47 +0200

    Working apollo server

---
+++ server/src/schema.js
@@ -0,0 +1,31 @@
+const {gql} = require('apollo-server');
+const typeDefs = gql`
+
+type Query {
+    "Get tracks for our homepage grid"
+    tracksForHome: [Track!]!
+}
+
+type Track {
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main author"    
+    author: Author!
+    "The track's main illustration to display in track card or track page detail"
+    thumbnail: String!
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+}
+
+type Author{
+    id: ID!
+    name: String!
+    photo: String
+
+}
+  # Schema definitions go here
+`;
+module.exports = typeDefs;
